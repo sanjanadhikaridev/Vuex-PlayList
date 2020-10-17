fe64dd735cfbcdbe7958ae174d785ce5df16c6e0
sanjanadhikaridev <maximus0427@outlook.com>
2020-10-18 02:53:19 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 
 //components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 //apollo client setup
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql'
@@ -15,6 +16,7 @@ function App() {
             <div id="main">
                 <h1>Ninja's Reading List</h1>
                 <BookList />
+                <AddBook />
             </div>
         </ApolloProvider>
     );
