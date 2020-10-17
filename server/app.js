c2091a2f4f84611c66bc54db4d4c365e6e78cd03
sanjanadhikaridev <maximus0427@outlook.com>
2020-10-17 18:50:58 +0800

    complete

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mongodb atlas
+mongoose.connect(
+    'mongodb+srv://admin:admin@cluster0.pfjcn.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }
+)
+mongoose.connection.once('open', ()=>{
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
